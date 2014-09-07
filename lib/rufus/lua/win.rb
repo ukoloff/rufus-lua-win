a0556b205189e108a310b8fbb518b94f4f1c97ab
Stas Ukolov <ukoloff@gmail.com>
2014-09-07 15:49:23 +0600

    Use Lua binaries & libraries

--- lib/rufus/lua/win.rb
+++ lib/rufus/lua/win.rb
@@ -3,7 +3,17 @@ require "rufus/lua/win/version"
 module Rufus
   module Lua
     module Win
-      # Your code goes here...
+      def self.inject
+        dll = File.expand_path '../../../../vendor/lua/bin/liblua.dll', __FILE__
+        rocks = File.expand_path '../../lib/lua', dll
+
+        ENV['LUA_LIB']=dll
+        ENV['LUA_PATH']=['', '/init'].map{|x| File.expand_path "?#{x}.lua", rocks}*';'
+        ENV['LUA_CPATH']=File.expand_path "?.dll", rocks
+        require 'rufus-lua'
+      end
+
+      inject
     end
   end
 end
