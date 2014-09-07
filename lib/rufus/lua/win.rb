e7c24bcb2e6445e10b1b3f206d643277da7d7178
Stas Ukolov <ukoloff@gmail.com>
2014-09-07 13:16:06 +0600

    New gem

---
+++ lib/rufus/lua/win.rb
@@ -0,0 +1,9 @@
+require "rufus/lua/win/version"
+
+module Rufus
+  module Lua
+    module Win
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rufus/lua/win/version.rb
@@ -0,0 +1,7 @@
+module Rufus
+  module Lua
+    module Win
+      VERSION = "0.0.1"
+    end
+  end
+end
